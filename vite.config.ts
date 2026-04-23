d2d66d33052e85cc35950405b317c0cac9956950
Takaesu._.p <zinn.1019t@gmail.com>
2026-04-23 15:18:45 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // 独自ドメインのルートで動かすための設定
-  base: './', 
+  base: './', // 独自ドメイン・サブディレクトリ両対応
   build: {
-    // Actionsが探しにいくフォルダ名と一致させる
-    outDir: 'dist',
+    outDir: 'dist'
   }
 })
